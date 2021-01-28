ce8717286cf23982be297c0dfe27f79bc36dbaa7
ZHAO Jinxiang <xiaoxiangmoe@gmail.com>
2021-01-29 00:40:23 +0800

    feat: use readonly in return value type (#15)

--- src/use-is-mounted-ref.ts
+++ src/use-is-mounted-ref.ts
@@ -1,12 +1,14 @@
-import { useRef, useEffect, MutableRefObject } from 'react';
+import { useRef, useEffect } from 'react';
 
-function useIsMountedRef(): MutableRefObject<boolean> {
+function useIsMountedRef(): { readonly current: boolean } {
   const isMountedRef = useRef(false);
 
   useEffect(() => {
     isMountedRef.current = true;
 
-    return () => (isMountedRef.current = false);
+    return () => {
+      isMountedRef.current = false;
+    };
   }, []);
 
   return isMountedRef;
